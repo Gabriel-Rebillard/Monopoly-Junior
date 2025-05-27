ba0f54e8b94031b47cb88e613a05bbc6169c8d7a
mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2025-05-27 16:54:07 +0200

    Create Joueur.cpp

---
+++ Monopoly-Junior/Monopoly-Junior/Joueur.cpp
@@ -0,0 +1,42 @@
+#include "Joueur.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+
+
+Joueur::Joueur(const std::string& playerName) : name(playerName), money(31), position(0)
+{
+}
+
+std::string Joueur::getName()
+{
+return name;
+}
+
+void Joueur::modifyMoney(short montant)
+{
+money += montant;
+}
+
+void Joueur::setPosition(short pos)
+{
+position = pos % 32;
+}
+
+short Joueur::getPosition()
+{
+return position;
+}
+
+short Joueur::rollDice()
+{
+return (std::rand() %6) +1;
+}
+
+short Joueur::getMoney()
+{
+return money;
+}
