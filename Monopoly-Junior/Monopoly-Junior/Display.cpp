ae1208ac7dd3d257229f3fcadffd532014aba4ba
mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2025-06-03 16:54:00 +0200

    Update Display.cpp

--- Monopoly-Junior/Monopoly-Junior/Display.cpp
+++ Monopoly-Junior/Monopoly-Junior/Display.cpp
@@ -7,53 +7,49 @@
 #include <ctime>
 
 
-void Display::write_case()
+void Display::write_case(Joueur& joueur, Case& case)
 {
-	std::cout << getName() << "Vous etes sur la case" << getPosition() << std::endl;
+	std::cout << joueur.getName() << "Vous etes sur la case" << case.getPosition() << std::endl;
 }
 
-void Display::write_chance()
+void Display::write_chance(Joueur& joueur)
 {
-	std::cout << getName() << "Vous etes sur une carte chance!!!" <<  << std::endl;
+	std::cout << joueur.getName() << "Vous etes sur une carte chance!!!" << std::endl;
 }
 
 
-void Display::write_intro()
+
+void Display::write_buy(Joueur& joueur)
 {
-	std::cout << "Bienvenue dans Monopoly junior" << std::endl;
+	std::cout << joueur.getName() << "Voulez vous acheter cette attraction"  << std::endl;
 }
 
-void Display::write_buy()
+void Display::write_prison(Joueur& joueur)
 {
-	std::cout << getName() << "Voulez vous acheter cette attraction"  << std::endl;
+	std::cout << joueur.getName() << "Vous etes sur la case aller en prison paie 3$ pout prendre le bus qui vous menera jusqu'a la case prison sans prendre largent de la case départ"  << std::endl;
 }
 
-void Display::write_prison()
+void Display::write_gare(Joueur& joueur)
 {
-	std::cout << getName() << "Vous etes sur la case aller en prison paie 3$ pout prendre le bus qui vous menera jusqu'a la case prison sans prendre largent de la case départ"  << std::endl;
+	std::cout << joueur.getName() << "Vous etes dans la gare relancé le dé"  << std::endl;
 }
 
-void Display::write_gare()
+void Display::write_caseDepart(Joueur& joueur)
 {
-	std::cout << getName() << "Vous etes dans la gare relancé le dé"  << std::endl;
+	std::cout << joueur.getName() << "Vous etes sur la case départ recevez 2$" << std::endl;
 }
 
-void Display::write_caseDepart()
+void Display::write_fortune(Joueur& joueur)
 {
-	std::cout << getName() << "Vous etes sur la case départ recevez 2$" << std::endl;
+	std::cout << joueur.getName() << "Vous etes sur la case fortune prenez tout l'argent bien jouer" << std::endl;
 }
 
-void Display::write_fortune()
+void Display::write_newtour(Joueur& joueur)
 {
-	std::cout << getName() << "Vous etes sur la case fortune prenez tout l'argent bien jouer" << std::endl;
+	std::cout << joueur.getName() << "Vous avez fait un nouveau tour du plateau recevez 2$" << std::endl;
 }
 
-void Display::write_newtour()
+void Display::write_taxe(Joueur& joueur)
 {
-	std::cout << getName() << "Vous avez fait un nouveau tour du plateau recevez 2$" << std::endl;
-}
-
-void Display::write_taxe()
-{
-	std::cout << getName() << "vous etes sur une case de spectacle payer 2$" << std::endl;
+	std::cout << joueur.getName() << "vous etes sur une case de spectacle payer 2$" << std::endl;
 }
