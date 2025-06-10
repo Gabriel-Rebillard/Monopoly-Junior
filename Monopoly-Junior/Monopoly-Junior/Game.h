12294172ccbd5ee774153f92aaf9b0c09c4b3714
Gabriel-Rebillard <gabriel_rebillard@etu.u-bourgogne.fr>
2025-06-10 11:56:25 +0200

    Update Game.h

--- Monopoly-Junior/Monopoly-Junior/Game.h
+++ Monopoly-Junior/Monopoly-Junior/Game.h
@@ -4,21 +4,26 @@
 #include <vector>
 #include <fstream>
 #include "Card.h"
-#include "Attraction.h"
 #include "Joueur.h"
+
+#include "Attraction.h"
 #include "Fortune.h"
+
+#include "Display.h"
 
 #define CHANCE_FILE_NAME "chance.txt"
 #define CASES_FILE_NAME "cases.txt"
+#define CASE_NUMBER 32
 
 class Game {
 private:
-    std::vector<Case> cases;
+    std::vector<Case*> cases;
     std::list<Card> cards;
     std::vector<Player> players;
 
-  
-    Case& getCase(short position);
+    Display display;
+
+    Case* getCase(short position);
     void payerLoyer();
     void placerStand();
     void tirerCarte();
