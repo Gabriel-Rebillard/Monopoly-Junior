8c6fa3002a5d26aadebeeb0f9bc36bb7ba1cf2b1
Gabriel-Rebillard <gabriel_rebillard@etu.u-bourgogne.fr>
2025-06-10 12:00:44 +0200

    Update Case.h

--- Monopoly-Junior/Monopoly-Junior/Case.h
+++ Monopoly-Junior/Monopoly-Junior/Case.h
@@ -7,7 +7,17 @@ protected:
 	std::string name;
 
 public:
+	Case();
 	Case(const std::string& name);
-	std::string& getName();
+	std::string getName();
+
+	//Attraction methodes
+	virtual short getPrice();
+	virtual void setOwner(Joueur& own);
+	virtual Joueur* getOwner();
+	
+	//Fortune methodes
+	virtual short getMoney();
+	virtual void addMoney(short amount);
 };
 
