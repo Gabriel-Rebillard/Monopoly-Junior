477c1dced94df20cb70bd8ea0fed5064d27c990d
Gabriel-Rebillard <gabriel_rebillard@etu.u-bourgogne.fr>
2025-06-10 10:16:27 +0200

    Update Display.cpp

--- Monopoly-Junior/Monopoly-Junior/Display.cpp
+++ Monopoly-Junior/Monopoly-Junior/Display.cpp
@@ -1,55 +1,72 @@
 #include "Display.h"
-#include "Joueur.h"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cstdlib>
-#include <ctime>
 
-
-void Display::write_case(Joueur& joueur, Case& case)
+void Display::displayCase(Case& c)
 {
-	std::cout << joueur.getName() << "Vous etes sur la case" << case.getPosition() << std::endl;
+	std::cout << "\t -> Vous etes sur la case " << c.getName() << std::endl;
+
+	short length = c.getName().length() + 2 - (c.getName().find('é') < 100 ? 1 : 0) - (c.getName().find('â') < 100 ? 1 : 0);
+	
+	std::cout << "\t╔";
+	for (short i = 0; i < length; i++)
+		std::cout << "═";
+	std::cout << "╗\n\t║ " << c.getName() << " ║\n\t╚";
+	for (short i = 0; i < length; i++)
+		std::cout << "═";
+	std::cout << "╝\n\n";
 }
 
-void Display::write_chance(Joueur& joueur)
+void Display::displayCard(const std::string& text)
 {
-	std::cout << joueur.getName() << "Vous etes sur une carte chance!!!" << std::endl;
+	std::cout << "\t ==> Vous etes sur une carte chance !!!\n\t\tCarte :\n" ;
+
+	short length = text.length() + 2 - (text.find('é') < 100 ? 1 : 0) - (text.find('ç') < 100 ? 1 : 0);
+	std::cout << "\t┌";
+	for (short i = 0; i < length; i++)
+		std::cout << "─";
+	std::cout << "┐\n\t│ " << text << " │\n\t└";
+	for (short i = 0; i < length; i++)
+		std::cout << "─";
+	std::cout << "┘\n\n";
+
 }
 
-
-
-void Display::write_buy(Joueur& joueur)
+bool Display::displayBuyChoice()
 {
-	std::cout << joueur.getName() << "Voulez vous acheter cette attraction"  << std::endl;
+	char choice;
+
+	do {
+		std::cout << "\tVoulez vous acheter cette attraction ? (0 : non / 1 : oui) : " << std::endl;
+		std::cin >> choice;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	} while (choice != '0' && choice != '1');
+
+	return choice - '0';
 }
 
-void Display::write_prison(Joueur& joueur)
+void Display::displayCoffee(Joueur& joueur)
 {
-	std::cout << joueur.getName() << "Vous etes sur la case aller en prison paie 3$ pout prendre le bus qui vous menera jusqu'a la case prison sans prendre largent de la case départ"  << std::endl;
+	std::cout << joueur.getName() << " : Vous êtes sur la case aller au café :  payez 3$ pout prendre le bus qui vous menera jusqu'a la case café sans prendre l'argent de la case départ."  << std::endl;
 }
 
-void Display::write_gare(Joueur& joueur)
+void Display::displayTrainStation(Joueur& joueur)
 {
-	std::cout << joueur.getName() << "Vous etes dans la gare relancé le dé"  << std::endl;
+	std::cout << joueur.getName() << " : Vous êtes dans une gare, relancez le dé !"  << std::endl;
 }
 
-void Display::write_caseDepart(Joueur& joueur)
+void Display::DisplayStartCase(Joueur& joueur, bool passed)
 {
-	std::cout << joueur.getName() << "Vous etes sur la case départ recevez 2$" << std::endl;
+	if (passed)
+		std::cout << joueur.getName() << " : Vous avez passé la case départ et reçu 2$." << std::endl;
+	else
+		std::cout << joueur.getName() << " : Vous êtes sur la case départ, recevez 4$ !" << std::endl;
 }
 
-void Display::write_fortune(Joueur& joueur)
+void Display::displayFortune(Joueur& joueur, short amount)
 {
-	std::cout << joueur.getName() << "Vous etes sur la case fortune prenez tout l'argent bien jouer" << std::endl;
+	std::cout << joueur.getName() << " : Vous êtes sur la case fortune, récupérez les " << amount << "$ qui s'y trouvent !" << std::endl;
 }
 
-void Display::write_newtour(Joueur& joueur)
+void Display::displayShowCase(Joueur& joueur)
 {
-	std::cout << joueur.getName() << "Vous avez fait un nouveau tour du plateau recevez 2$" << std::endl;
-}
-
-void Display::write_taxe(Joueur& joueur)
-{
-	std::cout << joueur.getName() << "vous etes sur une case de spectacle payer 2$" << std::endl;
+	std::cout << joueur.getName() << " : Vous êtes sur une case de spectacle, payez 2$." << std::endl;
 }
