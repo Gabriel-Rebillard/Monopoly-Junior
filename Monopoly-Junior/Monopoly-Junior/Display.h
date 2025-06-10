4b98d4995c463bf6ab7b3355105a293c2c1ad6e2
Gabriel-Rebillard <gabriel_rebillard@etu.u-bourgogne.fr>
2025-06-10 10:14:51 +0200

    Update Display.h

--- Monopoly-Junior/Monopoly-Junior/Display.h
+++ Monopoly-Junior/Monopoly-Junior/Display.h
@@ -1,23 +1,19 @@
 #pragma once
-#include "Joueur.h"
 #include <iostream>
-#include <vector>
 #include <string>
-#include <cstdlib>
-#include <ctime>
-#include "Game.h"
+
+#include "Joueur.h"
+#include "Case.h"
+
 class Display
 {
 public:
-	void write_case(Joueur& joueur, Case& case);
-	void write_chance(Joueur& joueur);
-	void write_buy(Joueur& joueur);
-	void write_prison(Joueur& joueur);
-	void write_gare(Joueur& joueur);
-	void write_caseDepart(Joueur& joueur);
-	void write_fortune(Joueur& joueur);
-	void write_newtour(Joueur& joueur);
-	void write_taxe(Joueur& joueur);
-
-
+	void displayCase(Case& c);
+	void displayCard(const std::string& text);
+	bool displayBuyChoice();
+	void displayCoffee(Joueur& joueur);
+	void displayTrainStation(Joueur& joueur);
+	void DisplayStartCase(Joueur& joueur, bool passed);
+	void displayFortune(Joueur& joueur, short amount);
+	void displayShowCase(Joueur& joueur);
 };
