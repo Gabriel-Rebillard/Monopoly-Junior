2ae2710446d8463733f8da118ec67324fe52dfe0
Gabriel-Rebillard <gabriel_rebillard@etu.u-bourgogne.fr>
2025-06-17 22:07:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include "Joueur.h"
 
 int main(void) {
+	// Permet à la console d'afficher les caractère spéciaux et accentués
 	SetConsoleOutputCP(65001);
+
+	// Rendre l'aléatoire non répétitif à chaque exécution du programme
 	srand((unsigned)time(NULL));
 
 	Game game;
 
-	char wait_char;
+	char wait_char;	// Caractère utilisé pour attendre le joueur lors d'une fin de tour par exemple
+	
 	std::cout << "\n\t\tMONOPOLY JUNIOR Version console";
 	std::cout << "\n\nCommencer la partie ? (appuyez sur une touche puis Entrée) :";
-	std::cin >> wait_char;
+	std::cin >> wait_char;	// "wait_char" est ici utilisé pour attendre que le joueur lance la partie
 
 
 
@@ -22,7 +26,7 @@ int main(void) {
 	do {
 		std::cout << "Combien de joueurs êtes vous ? (2 ~ 4) : ";
 		std::cin >> resp;
-		nbPlayers = resp.at(0);
+		nbPlayers = resp.at(0);	// Récupérer le premier caractère saisi
 
 		// Vérifier la saisie
 		if (nbPlayers != '2' && nbPlayers != '3' && nbPlayers != '4')
@@ -30,7 +34,7 @@ int main(void) {
 
 	} while (nbPlayers != '2' && nbPlayers != '3' && nbPlayers != '4');
 
-	nbPlayers -= '0';
+	nbPlayers -= '0';	// Convertir le caractère en entier
 
 
 
@@ -43,10 +47,13 @@ int main(void) {
 
 
 	// Boucle de jeu
-	while (game.nextTurn()){
+	while (game.nextTurn()){	// nextTurn() renvoi 1 tant qu'il y'a au moins deux joueurs encore en jeu
 		std::cout << "\n\n\nProchain joueur ? (appuyez sur une touche puis Entrée) : ";
 		std::cin >> wait_char;
 	}
 
+	std::cout << "\n\n\nMerci d'avoir joué ! (appuyez sur une touche puis Entrée pour arreter le programme) : ";
+	std::cin >> wait_char;
+	
 	return 0;
 }
