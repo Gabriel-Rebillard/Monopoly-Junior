1ce8d1ade2b79850eca8d9527cd0ff0b773365da
mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2025-06-17 17:50:25 +0200

    Update Fortune.h

--- Fortune.h
+++ Fortune.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "Case.h"
 
+// Classe Fortune : représente une case spéciale où de l'argent peut s'accumuler
 class Fortune : public Case
 {
 private:
-	short money;
+	short money;  // Montant d'argent stocké sur la case
 
 public:
+	// Constructeur : initialise le nom de la case et l'argent à zéro
 	Fortune(const std::string& name);
 
+	// Récupère l'argent disponible sur la case et le remet à zéro
 	short getMoney();
+
+	// Ajoute un montant d'argent à la case (toujours en valeur positive)
 	void addMoney(short amount);
 };
-
