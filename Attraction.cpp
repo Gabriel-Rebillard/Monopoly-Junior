ef5c378f60ccaa8e3237b4b18df80ec999b660f2
mgt689 <Papa-Maguette_Diop@etu.u-bourgogne.fr>
2025-06-17 17:18:07 +0200

    Update Attraction.cpp

--- Attraction.cpp
+++ Attraction.cpp
@@ -1,21 +1,26 @@
 #include "Attraction.h"
 
+// Constructeur de la classe Attraction
+// Initialise l'attraction avec un nom, un prix et un propriétaire par défaut ("None")
 Attraction::Attraction(const std::string& name, short price)
-	: price(price), Case(name), owner(new Joueur("None"))
+    : price(price), Case(name), owner(new Joueur("None")) // Initialisation des membres
 {
 }
 
+// Retourne le prix de l'attraction
 short Attraction::getPrice()
 {
-	return price;
+    return price;
 }
 
+// Définit le propriétaire de l'attraction
 void Attraction::setOwner(Joueur& own)
 {
-	owner = &own;
+    owner = &own; // On pointe vers le joueur passé en paramètre
 }
 
+// Retourne un pointeur vers le propriétaire de l'attraction
 Joueur* Attraction::getOwner()
 {
-	return owner;
+    return owner;
 }
